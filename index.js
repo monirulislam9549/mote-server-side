f28ddf5974b7ab058fcd99c6dac2d75913917d2a
monirulislam9549 <monirulislam9549@gmail.com>
2022-05-27 11:34:30 +0600

    connection to client

--- index.js
+++ index.js
@@ -15,23 +15,25 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nysre.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri);
 
-
-async function () {
+async function run() {
     try {
         await client.connect();
         const partsCollection = client.db('bike-mart').collection('parts');
 
-
         app.get('/parts', async (req, res) => {
             const query = {};
             const cursor = partsCollection.find(query)
+            const parts = await cursor.toArray();
+            res.send(parts)
         })
     }
     finally {
 
     }
 }
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Anything from motor bike')
