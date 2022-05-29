20e7c917f9000f2042cfa66557b1dc7610010ade
monirulislam9549 <monirulislam9549@gmail.com>
2022-05-29 07:16:46 +0600

    close cpnsole

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nysre.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri);
 
 async function run() {
     try {
@@ -28,6 +27,8 @@ async function run() {
             const parts = await cursor.toArray();
             res.send(parts)
         })
+
+
     }
     finally {
 
